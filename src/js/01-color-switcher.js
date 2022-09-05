a8a28e3b7a15bd81ce0a69cb64803d05914ae5cb
BigSiCorp <si.kocorp@gmail.com>
2022-09-05 21:54:24 +0300

    add first task

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,26 @@
+const startBtn = document.querySelector('button[data-start]');
+const stopBtn = document.querySelector('button[data-stop]');
 
+stopBtn.disabled = true;
+startBtn.addEventListener('click', makeStartBtnsDisable);
+stopBtn.addEventListener('click', makeStopBtnsDisable);
+let changeColor;
+
+function makeStartBtnsDisable(event) {
+  event.target.disabled = true;
+  stopBtn.disabled = false;
+
+  changeColor = setInterval(() => {
+    document.body.style.backgroundColor = getRandomHexColor();
+  }, 1000);
+}
+
+function makeStopBtnsDisable(event) {
+  event.target.disabled = true;
+  startBtn.disabled = false;
+  clearInterval(changeColor);
+}
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
